6c1c6837c935c3e22a9ce654b64f38f74fdf9f42
Omair Iqbal <omair95@protonmail.com>
2022-02-24 22:16:28 +0000

    Input read file added

--- src/com/technicalTest/Part2.java
+++ src/com/technicalTest/Part2.java
@@ -1,5 +1,9 @@
 package com.technicalTest;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Part2 {
 
     public static int lightsOn = 0;
@@ -27,13 +31,30 @@ public class Part2 {
     public static void main(String[] args) {
         int[][] lights = new int[1000][1000];
 
-        lightAction(lights, 0, 0, 999, 999, "on");
-        System.out.println("Number of lights ON = " + lightsOn);
+        try {
+            File myObj = new File("resources/input.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                Scanner coordinates = new Scanner(data).useDelimiter("\\D+");
+                String action = null;
 
-        lightAction(lights, 499, 499, 500, 500, "off");
-        System.out.println("Number of lights ON = " + lightsOn);
+                if (data.contains("on")) action = "on";
+                else if (data.contains("off")) action = "off";
+                else if (data.contains("toggle")) action = "toggle";
 
-        lightAction(lights, 0, 499, 999, 500, "toggle");
-        System.out.println("Number of lights ON = " + lightsOn);
+                int a = coordinates.nextInt();
+                int b = coordinates.nextInt();
+                int c = coordinates.nextInt();
+                int d = coordinates.nextInt();
+
+                lightAction(lights, a, b, c, d, action);
+                System.out.println("Number of lights ON = " + lightsOn);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
